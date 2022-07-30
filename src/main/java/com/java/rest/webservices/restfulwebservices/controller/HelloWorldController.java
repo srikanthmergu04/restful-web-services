f3d14f3fbbee720089857b09918d9639621ebb35
Srikanth Mergu <srikanth.mergu@teradata.com>
2022-07-30 12:56:07 +0530

    created basic GET API.

--- src/main/java/com/java/rest/webservices/restfulwebservices/controller/HelloWorldController.java
+++ src/main/java/com/java/rest/webservices/restfulwebservices/controller/HelloWorldController.java
@@ -1,7 +1,10 @@
 package com.java.rest.webservices.restfulwebservices.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.java.rest.webservices.restfulwebservices.model.Greeting;
 
 @RestController
 public class HelloWorldController {
@@ -9,6 +12,14 @@ public class HelloWorldController {
 	@GetMapping("/hello-world")
 	public String greeting() {
 		return "Welcome";
+	}
+
+	@GetMapping("/hello-world/{name}")
+	public Greeting greetingMsg(@PathVariable String name) {
+
+		Greeting greetingmsg = new Greeting();
+		greetingmsg.setMessage("Hello, " + name);
+		return greetingmsg;
 	}
 
 }
---
+++ src/main/java/com/java/rest/webservices/restfulwebservices/model/Greeting.java
@@ -0,0 +1,15 @@
+package com.java.rest.webservices.restfulwebservices.model;
+
+public class Greeting {
+
+	private String message;
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+}
