ec8af07e538d6a8174fdc146e2e305898da9d3e7
Srikanth Mergu <srikanth.mergu@teradata.com>
2022-07-30 18:59:00 +0530

    Implemented Internationalization.

--- src/main/java/com/java/rest/webservices/restfulwebservices/controller/HelloWorldController.java
+++ src/main/java/com/java/rest/webservices/restfulwebservices/controller/HelloWorldController.java
@@ -1,5 +1,8 @@
 package com.java.rest.webservices.restfulwebservices.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +11,9 @@ import com.java.rest.webservices.restfulwebservices.model.Greeting;
 
 @RestController
 public class HelloWorldController {
+
+	@Autowired
+	private MessageSource messageSource;
 
 	@GetMapping("/hello-world")
 	public String greeting() {
@@ -19,6 +25,12 @@ public class HelloWorldController {
 
 		Greeting greetingmsg = new Greeting(name);
 		return greetingmsg;
+	}
+
+	@GetMapping("/msg-internationalized")
+	public String greetingInternatioanlized() {
+		return messageSource.getMessage("good.morning.message", null, "Default Message",
+				LocaleContextHolder.getLocale());
 	}
 
 }
